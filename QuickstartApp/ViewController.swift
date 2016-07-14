997faff13dbbc35b75036948c5e5f8f8e4158e87
Johnny Cao <johnny@soapboxhq.com>
2016-07-14 15:27:44 -0400

    Initial Commit

---
+++ QuickstartApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickstartApp
+//
+//  Created by Luchao Cao on 2016-07-14.
+//  Copyright © 2016 com.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
