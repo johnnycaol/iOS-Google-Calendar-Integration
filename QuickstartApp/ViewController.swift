b9c535a271d49b1b3e44b7c3f4d89b9af0c4d0f3
Johnny Cao <johnny@soapboxhq.com>
2016-07-20 13:41:45 -0400

    add google calendar libraries

--- QuickstartApp/ViewController.swift
+++ QuickstartApp/ViewController.swift
@@ -6,20 +6,168 @@
 //  Copyright © 2016 com.example. All rights reserved.
 //
 
+import GoogleAPIClient
+import GTMOAuth2
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let kKeychainItemName = "Google Calendar API"
+    private let kClientID = "963744635405-v35a7e2f5gdji4jonsqqlp26nng5k4hh.apps.googleusercontent.com"
+    
+    // If modifying these scopes, delete your previously saved credentials by
+    // resetting the iOS simulator or uninstall the app.
+    private let scopes = [kGTLAuthScopeCalendarReadonly]
+    private let service = GTLServiceCalendar()
+    
+    let output = UITextView()
+    
+    // When the view loads, create necessary subviews
+    // and initialize the Google Calendar API service
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        output.frame = view.bounds
+        output.editable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        
+        view.addSubview(output);
+        
+        if let auth = GTMOAuth2ViewControllerTouch.authForGoogleFromKeychainForName(
+            kKeychainItemName,
+            clientID: kClientID,
+            clientSecret: nil) {
+            service.authorizer = auth
+        }
+        
     }
-
+    
+    // When the view appears, ensure that the Google Calendar API service is authorized
+    // and perform API calls
+    override func viewDidAppear(animated: Bool) {
+        if let authorizer = service.authorizer,
+            canAuth = authorizer.canAuthorize where canAuth {
+            fetchEvents()
+        } else {
+            presentViewController(
+                createAuthController(),
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    // Construct a query and get a list of upcoming events from the user calendar
+    func fetchEvents() {
+        let query = GTLQueryCalendar.queryForEventsListWithCalendarId("primary")
+        query.maxResults = 20
+        query.timeMin = GTLDateTime(date: NSDate(), timeZone: NSTimeZone.localTimeZone())
+        query.singleEvents = true
+        query.q = "1:1 johnny@soapboxhq.com graham@soapboxhq.com"//append the current user's email and the colleague's email
+        query.orderBy = kGTLCalendarOrderByStartTime //comment this out as it doesn't work with singleEvents = false
+        service.executeQuery(
+            query,
+            delegate: self,
+            didFinishSelector: "displayResultWithTicket:finishedWithObject:error:"
+        )
+    }
+    
+    // Display the start dates and event summaries in the UITextView
+    func displayResultWithTicket(
+        ticket: GTLServiceTicket,
+        finishedWithObject response : GTLCalendarEvents,
+                           error : NSError?) {
+        
+        if let error = error {
+            showAlert("Error", message: error.localizedDescription)
+            return
+        }
+        
+        var eventString = ""
+        
+        if let events = response.items() where !events.isEmpty {
+            for event in events as! [GTLCalendarEvent] {
+                
+                let start : GTLDateTime! = event.start.dateTime ?? event.start.date
+                let startString = NSDateFormatter.localizedStringFromDate(
+                    start.date,
+                    dateStyle: .ShortStyle,
+                    timeStyle: .ShortStyle
+                )
+                
+                let end : GTLDateTime! = event.end.dateTime ?? event.end.date
+                let endString = NSDateFormatter.localizedStringFromDate(
+                    end.date,
+                    dateStyle: .ShortStyle,
+                    timeStyle: .ShortStyle
+                )
+                
+                print(event)
+                print("Recurring Event Id: " + event.recurringEventId)
+                print("Description: " + event.summary)
+                print("Start: " + startString)
+                print("End: " + endString)
+                //print("Location: " + event.location)
+                print("\n")
+                eventString += "\(startString) - \(event.summary)\n"
+            }
+        } else {
+            eventString = "No upcoming events found."
+        }
+        
+        output.text = eventString
+    }
+    
+    
+    // Creates the auth controller for authorizing access to Google Calendar API
+    private func createAuthController() -> GTMOAuth2ViewControllerTouch {
+        let scopeString = scopes.joinWithSeparator(" ")
+        return GTMOAuth2ViewControllerTouch(
+            scope: scopeString,
+            clientID: kClientID,
+            clientSecret: nil,
+            keychainItemName: kKeychainItemName,
+            delegate: self,
+            finishedSelector: "viewController:finishedWithAuth:error:"
+        )
+    }
+    
+    // Handle completion of the authorization process, and update the Google Calendar API
+    // with the new credentials.
+    func viewController(vc : UIViewController,
+                        finishedWithAuth authResult : GTMOAuth2Authentication, error : NSError?) {
+        
+        if let error = error {
+            service.authorizer = nil
+            showAlert("Authentication Error", message: error.localizedDescription)
+            return
+        }
+        
+        service.authorizer = authResult
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Helper for showing an alert
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.Alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.Default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
